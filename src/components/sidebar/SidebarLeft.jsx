a626f1e6bf280fd10673255d56a4b06d5e09bf41
tarek <sitarek77@gmail.com>
2025-05-22 08:52:37 +0600

    desing

--- src/components/sidebar/SidebarLeft.jsx
+++ src/components/sidebar/SidebarLeft.jsx
@@ -5,101 +5,122 @@ export default function SidebarLeft() {
   const { t } = useTranslation();
 
   return (
-    <div className="space-y-4   px-2 pb-10 w-full h-full text-sm">
+    <div className="space-y-4 px-2 pb-10 w-full h-full text-sm">
       <div>
-        
-<h2 className="btn  w-full py-6  text-white font-bold bg-[#004080]">
+        <h2 className="bg-[#004080] py-6 w-full font-bold text-white btn">
           {t("the court")}
         </h2>
-   
-        
-        <ul className="space-y-2 mt-2 p-2">
+
+        <ul className="space-y-1 p-2">
           <li>
-             <NavLink to={"/history"} href="#"  className={({ isActive }) =>
-              isActive
-                ? "text-white w-full btn-neutral btn btn-sm "
-                : "w-full btn btn-outline btn-neutral btn-sm"
-            }>
+            <NavLink
+              to={"/history"}
+              href="#"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-white w-full btn-neutral btn btn-sm "
+                  : "w-full btn btn-outline btn-neutral btn-sm"
+              }
+            >
               {t("history")}
             </NavLink>
-            
           </li>
-         <li>
-             <NavLink to={"/calender"} href="#"  className={({ isActive }) =>
-              isActive
-                ? "text-white w-full btn-neutral btn btn-sm "
-                : "w-full btn btn-outline btn-neutral btn-sm"
-            }>
+          <li>
+            <NavLink
+              to={"/calender"}
+              href="#"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-white w-full btn-neutral btn btn-sm "
+                  : "w-full btn btn-outline btn-neutral btn-sm"
+              }
+            >
               {t("court calendar")}
             </NavLink>
-            
           </li>
-         <NavLink to={"/citizenCharter"} href="#"  className={({ isActive }) =>
+          <NavLink
+            to={"/citizenCharter"}
+            href="#"
+            className={({ isActive }) =>
               isActive
                 ? "text-white w-full btn-neutral btn btn-sm "
                 : "w-full btn btn-outline btn-neutral btn-sm"
-            }>
-              {t("citizen charter")}
-            </NavLink>
-             <NavLink to={"/rules"} href="#"  className={({ isActive }) =>
+            }
+          >
+            {t("citizen charter")}
+          </NavLink>
+          <NavLink
+            to={"/rules"}
+            href="#"
+            className={({ isActive }) =>
               isActive
                 ? "text-white w-full btn-neutral btn btn-sm "
                 : "w-full btn btn-outline btn-neutral btn-sm"
-            }>
-              {t("court rules")}
-            </NavLink>
-          
-          
-         
+            }
+          >
+            {t("court rules")}
+          </NavLink>
         </ul>
       </div>
 
       <div>
-        <h2 className="btn w-full   py-6 text-white font-bold bg-[#004080]">
+        <h2 className="bg-[#004080] w-full font-bold text-white btn">
           {t("resources")}
         </h2>
-        <ul className="space-y-2 mt-2 p-2 w-full">
-           <li>
-            <NavLink to={"/complain"} href="#"  className={({ isActive }) =>
-              isActive
-                ? "text-white w-full btn-neutral btn btn-sm "
-                : "w-full btn btn-outline btn-neutral btn-sm"
-            }>
+        <ul className="space-y-1 p-2 w-full">
+          <li>
+            <NavLink
+              to={"/complain"}
+              href="#"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-white w-full btn-neutral btn btn-sm "
+                  : "w-full btn btn-outline btn-neutral btn-sm"
+              }
+            >
               {t("lodge your complaint")}
             </NavLink>
           </li>
           <li>
-             <NavLink to={"/opinion"} href="#"  className={({ isActive }) =>
-              isActive
-                ? "text-white w-full btn-neutral btn btn-sm "
-                : "w-full btn btn-outline btn-neutral btn-sm"
-            }>
+            <NavLink
+              to={"/opinion"}
+              href="#"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-white w-full btn-neutral btn btn-sm "
+                  : "w-full btn btn-outline btn-neutral btn-sm"
+              }
+            >
               {t("opinion")}
             </NavLink>
-            
           </li>
           <li>
-             <NavLink to={"/gallery"} href="#" c className={({ isActive }) =>
-              isActive
-                ? "text-white w-full btn-neutral btn btn-sm "
-                : "w-full btn btn-outline btn-neutral btn-sm"
-            }>
+            <NavLink
+              to={"/gallery"}
+              href="#"
+              c
+              className={({ isActive }) =>
+                isActive
+                  ? "text-white w-full btn-neutral btn btn-sm "
+                  : "w-full btn btn-outline btn-neutral btn-sm"
+              }
+            >
               {t("photo gallery")}
             </NavLink>
-            
           </li>
           <li>
-             <NavLink to={"/contacts"} href="#"  className={({ isActive }) =>
-              isActive
-                ? "text-white w-full btn-neutral btn btn-sm "
-                : "w-full btn btn-outline btn-neutral btn-sm"
-            }>
+            <NavLink
+              to={"/contacts"}
+              href="#"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-white w-full btn-neutral btn btn-sm "
+                  : "w-full btn btn-outline btn-neutral btn-sm"
+              }
+            >
               {t("contacts")}
             </NavLink>
-            
           </li>
-
-          
         </ul>
       </div>
     </div>
