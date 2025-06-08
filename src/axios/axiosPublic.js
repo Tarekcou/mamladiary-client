bf68a64cdbef832844053dabb7c2c9ef09d417d9
tarek <sitarek77@gmail.com>
2025-06-08 13:22:26 +0600

    bulk sms

--- src/axios/axiosPublic.js
+++ src/axios/axiosPublic.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:5500", // Your backend URL
-  // baseURL: "https://mamladiary-server.vercel.app/", // Your backend URL
+  // baseURL: "http://localhost:5500", // Your backend URL
+  baseURL: "https://mamladiary-server.vercel.app/", // Your backend URL
 });
 
 export default axiosPublic;
